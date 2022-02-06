a18c7cb059800b498f860e1395f86b946d578999
mugac <71031347+mugac@users.noreply.github.com>
2022-02-06 22:57:33 +0100

    Add files via upload

---
+++ Boyer-Moore.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boyer_MooreovAlg
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            char[] txt = "ADCBACABCDAD".ToCharArray();
+            char[] pat = "ABC".ToCharArray();
+            Search(txt, pat);
+
+            Console.Read();
+        }
+
+        static int NO_OF_CHARS = 256;
+
+        static int Max(int a, int b) { return (a > b) ? a : b; }
+
+        static void BadCharHeuristic(char[] str, int size, int[] badchar)
+        {
+            int i;
+
+            for (i = 0; i < NO_OF_CHARS; i++)
+                badchar[i] = -1;
+
+            for (i = 0; i < size; i++)
+                badchar[(int)str[i]] = i;
+        }
+
+        static void Search(char[] txt, char[] pat)
+        {
+            int m = pat.Length;
+            int n = txt.Length;
+
+            int[] badchar = new int[NO_OF_CHARS];
+
+            BadCharHeuristic(pat, m, badchar);
+
+            int s = 0; 
+
+            while (s <= (n - m))
+            {
+                int j = m - 1;
+
+                while (j >= 0 && pat[j] == txt[s + j])
+                    j--;
+
+                if (j < 0)
+                {
+
+                    Console.WriteLine("Patern je na indexu: " + s);
+
+                    s += (s + m < n) ? m - badchar[txt[s + m]] : 1;
+
+                }
+                else
+                    s += Max(1, j - badchar[txt[s + j]]);
+            }
+        }
+    }
+}
---
+++ rabin-Karp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VyhledavaciAlgo
+{
+    internal class Program
+    {
+        public static int pocetChar = 256;
+        static void Main(string[] args)
+        {
+            Search("jak", "Ahoj jak se mas", 101);
+            Console.ReadLine();
+        }
+
+        static void Search(string patern, string text, int q)
+        {
+            int lenPat = patern.Length;
+            int lenTex = text.Length;
+            int hasPat = 0;
+            int hasTex = 0;
+            int i, j;
+            int h = 1;
+
+            for (i = 0; i < lenPat - 1; i++)
+            {
+                h = (h * pocetChar) % q;
+            }
+
+            for (i = 0; i < lenPat; i++)
+            {
+                hasPat = (pocetChar * hasPat + patern[i] )% q;
+                hasTex = (pocetChar * hasTex + text[i] )% q;
+            }
+
+            for (i = 0; i <= lenTex - lenPat; i++)
+            {
+                if (hasTex == hasPat)
+                {
+                    for (j = 0; j < lenPat; j++)
+                    {
+                        if (text[i + j] != patern[j])
+                        {
+                            break;
+                        }
+                    }
+
+                    if (j == lenPat)
+                    {
+                        Console.WriteLine("Patern byl nalezen na indexu " + i);
+                    }
+
+                    
+                }
+                if (i < lenTex - lenPat)
+                {
+                    hasTex = (pocetChar * (hasTex - text[i] * h) + text[i + lenPat] )% q;
+
+                    if (hasTex < 0)
+                    {
+                        hasTex += q;
+                    }
+                }
+            }
+
+        }
+    }
+}
